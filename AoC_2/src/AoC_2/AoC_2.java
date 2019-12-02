2b3bf9e12d902b99889b330f5dc256bc42317e35
Peter Lidman <Peter Lidman@37.123.132.62>
2019-12-02 20:03:37 +0100

    längtar efter svårare

--- AoC_2/src/AoC_2/AoC_2.java
+++ AoC_2/src/AoC_2/AoC_2.java
@@ -9,45 +9,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AoC_2 {
-	static List<Integer> _moduleMass = new ArrayList<Integer>();
+	static List<Integer> _intCode = new ArrayList<Integer>();
 
 	public static void main(String[] args) throws IOException {
+		int output = 19690720, result = 0;
+
 		readInput();
+		System.out.println("Del 1 = " + runProgram(12, 2));
 
-		System.out.println("f" + calc(12));
-		System.out.println("f" + calc(14));
-		System.out.println("f" + calc(1969));
-		System.out.println("f" + calc(100756));
-		System.out.println("Sum of fuel = " + del1());
-
-		System.out.println("f" + reccalc(14));
-		System.out.println("f" + reccalc(1969));
-		System.out.println("f" + reccalc(100756));
-		System.out.println("Sum of fuel = " + del2());
+		outerloop: for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				readInput();
+				result = runProgram(i, j);
+				if (output == result) {
+					System.out.println("Del 2 = " + (i * 100 + j));
+					break outerloop;
+				}
+			}
+		}
 	}
 
-	private static int reccalc(int a) {
-		return calc(a) > 0 ? calc(a) + reccalc(calc(a)) : 0;
+	private static int runProgram(int noun, int verb) throws FileNotFoundException, IOException {
+		_intCode.set(1, noun);
+		_intCode.set(2, verb);
+		int pc = 0;
+
+		while (executeOrQuit(pc)) {
+			pc += 4;
+		}
+		return _intCode.get(0);
 	}
 
-	private static int calc(int a) {
-		return a / 3 - 2;
-	}
-
-	public static int del1() {
-		return _moduleMass.stream().map(i -> calc(i)).reduce(Integer::sum).get();
-	}
-
-	public static int del2() {
-		return _moduleMass.stream().map(i -> reccalc(i)).reduce(Integer::sum).get();
+	private static boolean executeOrQuit(int a) {
+		if (_intCode.get(a) == 99) {
+			return false;
+		}
+		if (_intCode.get(a) == 1) {// add
+			_intCode.set(_intCode.get(a + 3), _intCode.get(_intCode.get(a + 1)) + _intCode.get(_intCode.get(a + 2)));
+		} else if (_intCode.get(a) == 2) {// mult
+			_intCode.set(_intCode.get(a + 3), _intCode.get(_intCode.get(a + 1)) * _intCode.get(_intCode.get(a + 2)));
+		} else {
+			System.out.println("Unknown opcode");
+		}
+		return true;
 	}
 
 	private static void readInput() throws FileNotFoundException, IOException {
-		String str;
-		FileInputStream in = new FileInputStream("C:\\git\\AoC2019\\AoC_1\\src\\AoC_1\\input.txt");
+		String str, splitStr[];
+		_intCode.clear();
+		FileInputStream in = new FileInputStream("C:\\git\\AoC2019\\AoC_2\\src\\AoC_2\\input.txt");
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		while ((str = br.readLine()) != null) {
-			_moduleMass.add(Integer.parseInt(str));
+			splitStr = str.split(",");
+			for (String s : splitStr) {
+				_intCode.add(Integer.parseInt(s));
+			}
 		}
 		br.close();
 	}
