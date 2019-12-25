05ee8abb44e99640dba6f0c7fbca4deb1f89a59c
Peter Lidman <Peter Lidman@37.123.132.62>
2019-12-25 15:36:39 +0100

    hade inte gått utan hjälp

--- AoC_22/src/AoC_22/AoC_22.java
+++ AoC_22/src/AoC_22/AoC_22.java
@@ -4,12 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AoC_22 {
 	static List<String> _shuffle = new ArrayList<>();
-
 	static List<Integer> _deckA = new ArrayList<Integer>();
 	static List<Integer> _deckB = new ArrayList<Integer>();
 	static boolean _isYourDeckA = true;// else it's B
@@ -23,6 +23,52 @@ public class AoC_22 {
 
 	public void run() {
 		del1();
+		del2();
+	}
+
+	private BigInteger seek(BigInteger deckSize, BigInteger timesShuffled, int position) {
+		BigInteger[] in = new BigInteger[] { new BigInteger("1"), new BigInteger("0") };
+
+		for (int i = _shuffle.size() - 1; i >= 0; i--) {
+			if (_shuffle.get(i).startsWith("deal into")) {
+				reverseDealIntoNewStack(in);
+			} else if (_shuffle.get(i).startsWith("deal with")) {
+				int with = Integer.valueOf(_shuffle.get(i).substring(19).trim());
+				reverseDealWithIncrement(in, deckSize, with);
+			} else {// cut
+				int cut = Integer.valueOf(_shuffle.get(i).substring(3).trim());
+				reverseCutNcards(in, cut);
+			}
+			in[0] = in[0].mod(deckSize);
+			in[1] = in[1].mod(deckSize);
+		}
+		BigInteger pow = in[0].modPow(timesShuffled, deckSize);
+
+		return pow.multiply(new BigInteger("" + position))
+				.add(in[1].multiply(pow.add(deckSize).subtract(new BigInteger("1"))).multiply(
+						in[0].subtract(new BigInteger("1")).modPow(deckSize.subtract(new BigInteger("2")), deckSize)))
+				.mod(deckSize);
+	}
+
+	private void reverseCutNcards(BigInteger[] in, Integer cut) {
+		in[1] = in[1].add(new BigInteger(cut.toString()));
+	}
+
+	private void reverseDealWithIncrement(BigInteger[] in, BigInteger deckSize, Integer with) {
+		BigInteger p = new BigInteger(with.toString()).modPow(deckSize.subtract(new BigInteger("2")), deckSize);
+		in[0] = in[0].multiply(p);
+		in[1] = in[1].multiply(p);
+	}
+
+	private void reverseDealIntoNewStack(BigInteger[] in) {
+		in[0] = in[0].multiply(new BigInteger("-1"));
+		in[1] = in[1].add(new BigInteger("1")).multiply(new BigInteger("-1"));
+	}
+
+	public void del2() {
+		readInput(22);
+		System.out
+				.println("del 2: " + seek(new BigInteger("119315717514047"), new BigInteger("101741582076661"), 2020));
 	}
 
 	private void init() {
@@ -45,8 +91,6 @@ public class AoC_22 {
 				_deckB.add(_deckA.get(i));
 			}
 			for (int i = 0; i < cut; i++) {
-//				System.out.println(i);
-//				System.out.println(cut);
 				_deckB.add(_deckA.get(i));
 			}
 			_isYourDeckA = false;
@@ -97,16 +141,12 @@ public class AoC_22 {
 		for (int i = 0; i < _shuffle.size(); i++) {
 			if (_shuffle.get(i).startsWith("deal into")) {
 				dealIntoNewStack();
-//				System.out.println("into");
 			} else if (_shuffle.get(i).startsWith("deal with")) {
 				int with = Integer.valueOf(_shuffle.get(i).substring(19).trim());
 				dealWithIncrement(with);
-//				System.out.println("with " + with);
-
 			} else {// cut
 				int cut = Integer.valueOf(_shuffle.get(i).substring(3).trim());
 				cutNcards(cut);
-//				System.out.println("cut " + cut);
 			}
 		}
 		if (_isYourDeckA) {
